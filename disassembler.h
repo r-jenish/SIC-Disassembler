41cd84f2358a27c8dec731cb6c3b8076e2c4e8e5
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-02-23 16:16:04 +0530

    Similar Format

--- disassembler.h
+++ disassembler.h
@@ -1,5 +1,6 @@
 #ifndef _DISASSEMBLER_H_
 #define _DISASSEMBLER_H_
+
 #include <string>
 
 void readinput(char *);
--- util.h
+++ util.h
@@ -1,5 +1,6 @@
 #ifndef _UTIL_H_
 #define _UTIL_H_
+
 #include <string>
 
 void fatalerror(std::string);
