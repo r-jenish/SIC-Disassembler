0a15456beceefae7a1e601495f8b2ddc5b444013
Rakholiya Jenish <rakholiyajenish.07@gmail.com>
2015-02-25 14:28:35 +0530

    Improved terminal output formatting in different colors and made them bold.

--- util.cpp
+++ util.cpp
@@ -3,40 +3,49 @@
 #include <iostream>
 #include <cstdlib>
 
+#define KNRM  "\x1B[0m"
+#define KRED  "\x1B[1;31m"
+#define KGRN  "\x1B[1;32m"
+#define KYEL  "\x1B[1;33m"
+#define KBLU  "\x1B[1;34m"
+#define KMAG  "\x1B[1;35m"
+#define KCYN  "\x1B[1;36m"
+#define KWHT  "\x1B[1;37m"
+
 void fatalerror(std::string err) {
-	std::cout << "- " << err << std::endl;
+	std::cout << KRED << "- " << err << std::endl;
 	displaylastmessage();
 	exit(-1);
 }
 
 void warningmessage(std::string war) {
-	std::cout << "! " << war << std::endl;
+	std::cout << KYEL << "! " << war << std::endl;
 }
 
 void infileopened() {
-	std::cout << "+ Object file successfully opened." << std::endl;
+	std::cout << KBLU <<  "+ Input object file successfully opened." << std::endl;
 }
 
 void infileclosed() {
-	std::cout << "+ Object file successfully closed." << std::endl;
+	std::cout << KGRN << "+ Input object file successfully closed." << std::endl;
 }
 
 void startingdisassembling() {
-	std::cout << "+ Starting disassembling." << std::endl;
+	std::cout << KBLU << "+ Starting disassembling." << std::endl;
 }
 
 void finishdisassembling() {
-	std::cout << "+ Finished disassembling and output has be written in ouputfile specified." << std::endl;
+	std::cout << KGRN << "+ Finished disassembling and output has be written in ouputfile specified." << std::endl;
 }
 
 void outfileopened() {
-	std::cout << "+ Output file successfully opened." << std::endl;
+	std::cout << KBLU << "+ Output file successfully opened." << std::endl;
 }
 
 void outfileclosed() {
-	std::cout << "+ Output file successfully closed." << std::endl;
+	std::cout << KGRN << "+ Output file successfully closed." << std::endl;
 }
 
 void displaylastmessage() {
-	std::cout << "=======Copyright (c) 2015 Rakholiya Jenish=======" << std::endl;
+	std::cout << KCYN << "=======Copyright (c) 2015 Rakholiya Jenish=======" << KNRM << std::endl;
 }
